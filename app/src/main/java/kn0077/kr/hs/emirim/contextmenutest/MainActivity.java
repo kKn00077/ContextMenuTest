405241a82ca303e1f4067a240a32fa06439d9517
Kim kyuna <kkn00077@gmail.com>
2017-05-25 19:33:07 +0900

    #4 event programming

--- app/src/main/java/kn0077/kr/hs/emirim/contextmenutest/MainActivity.java
+++ app/src/main/java/kn0077/kr/hs/emirim/contextmenutest/MainActivity.java
@@ -1,22 +1,33 @@
 package kn0077.kr.hs.emirim.contextmenutest;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    LinearLayout linear;
+    ImageView img1;
+    Button but2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button but1=(Button)findViewById(R.id.but1);
-        Button but2=(Button)findViewById(R.id.but2);
+        but2=(Button)findViewById(R.id.but2);
+        linear=(LinearLayout)findViewById(R.id.linear);
+        img1=(ImageView) findViewById(R.id.img1);
         registerForContextMenu(but1);
         registerForContextMenu(but2);
+
 
 
     }
@@ -35,5 +46,19 @@ public class MainActivity extends AppCompatActivity {
             menuInflater.inflate(R.menu.menu2,menu);
         }
 
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        super.onContextItemSelected(item);
+        switch(item.getItemId()){
+            case R.id.menu1_item_red : linear.setBackgroundColor(Color.RED);  return true;
+            case R.id.menu1_item_blue : linear.setBackgroundColor(Color.BLUE); return true;
+            case R.id.menu1_item_pink : linear.setBackgroundColor(Color.rgb(255,178,217)); return true;
+            case R.id.menu2_item_rotation : img1.setRotation(img1.getRotation()+90); return true;
+            case R.id.menu2_item_size : img1.setScaleX(img1.getScaleX()+0.5f); img1.setScaleY(img1.getScaleY()+0.5f); return true;
+
+        }
+        return false;
     }
 }
